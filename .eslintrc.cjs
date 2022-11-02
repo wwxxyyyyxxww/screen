7717f51c2cc627c6e172bd405c268251efec3b9a
xiaoyuer <xiaoyuer6964@163.com>
2022-11-02 11:06:45 +0800

    feat: add husky

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,17 +3,17 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-//   extends: [
-//     "plugin:vue/vue3-essential",
-//     "standard-with-typescript",
-//     "plugin:prettier/recommended",
-//     "prettier",
-//   ],
-//   overrides: [],
-//   parserOptions: {
-//     ecmaVersion: "latest",
-//     sourceType: "module",
-//   },
-//   plugins: ["@typescript-eslint", "prettier"],
-//   rules: {},
+  extends: [
+    "plugin:vue/vue3-essential",
+    "standard-with-typescript",
+    "plugin:prettier/recommended",
+    "prettier",
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint", "prettier"],
+  rules: {},
 };
